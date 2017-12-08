33e1ab488b6a738c3f80176c358d77909e83b39c
merlink01 <merlink@gmx.de>
2017-12-08 16:54:03 +0100

    A little cleanup

--- fs/expose/xmlrpc/xmlrpc.py
+++ fs/expose/xmlrpc/xmlrpc.py
@@ -62,15 +62,12 @@ class RPCFSInterface(object):
                         "isempty",
                         "move",
                         "movedir",
-                        
                         "scandir",
                         "settimes",
                         "settext",
                         "setinfo",
                         "match",
-                        # ~ "getsyspath",
                         "gettext",
-                        # ~ "isclosed",
                         "copy",
                         "copydir",
                         "desc",
@@ -94,18 +91,17 @@ class RPCFSInterface(object):
 
 
         if not method in self._allowed_methods:
-            print('Server',method,params,'-->','Unsupported')
+            # ~ print('Server',method,params,'-->','Unsupported')
             raise errors.Unsupported
         
         
         # ~ return func(*params)
-        #Debugging
+
         try: 
             func = getattr(self.fs, method)
             params = list(params)
 
             if six.PY2:
-                # ~ if not
                 if method in ['match']:
                     params[1] = params[1].decode('utf-8')
                 else:
@@ -127,7 +123,7 @@ class RPCFSInterface(object):
                 try:
                     params[1] = params[1].data
                 except:
-                    print('Server',method,params,'-->','TypeError: Need an xmlrpc.Binary object')
+                    # ~ print('Server',method,params,'-->','TypeError: Need an xmlrpc.Binary object')
                     raise TypeError('Need an xmlrpc.Binary object')
                     
             if method in ['settimes']:
@@ -150,148 +146,19 @@ class RPCFSInterface(object):
                 
             if method in ['getmeta']:
                 if 'invalid_path_chars' in returndata:
-                    # ~ print '+++++++++++++++++++++++',returndata
                     returndata['invalid_path_chars'] = xmlrpclib.Binary(returndata['invalid_path_chars'].encode('utf-8'))
-            try:
-                print('Server',method,params,'-->',returndata)
-            except:
-                pass
+            # ~ try:
+                # ~ print('Server',method,params,'-->',returndata)
+            # ~ except:
+                # ~ pass
             return returndata
         except:
-            import traceback
-            print('############## Traceback from Server ####################')
+            # ~ import traceback
+            # ~ print('############## Traceback from Server ####################')
             # ~ print('Server',method,params,'-->','Error')
-            traceback.print_exc()
-            print('############## Traceback from Server ####################')
+            # ~ traceback.print_exc()
+            # ~ print('############## Traceback from Server ####################')
             raise
-
-    # ~ def encode_path(self, path):
-        # ~ """Encode a filesystem path for sending over the wire.
-
-        # ~ Unfortunately XMLRPC only supports ASCII strings, so this method
-        # ~ must return something that can be represented in ASCII.  The default
-        # ~ is base64-encoded UTF-8.
-        # ~ """
-        # ~ return path#six.text_type(base64.b64encode(path.encode("utf8")), 'ascii')
-
-    # ~ def decode_path(self, path):
-        # ~ """Decode paths arriving over the wire."""
-        # ~ return six.text_type(base64.b64decode(path.encode('ascii')), 'utf8')
-
-    # ~ def getmeta(self, meta_name):
-        # ~ meta = self.fs.getmeta(meta_name)
-        # ~ if isinstance(meta, basestring):
-            # ~ meta = self.decode_path(meta)
-        # ~ return meta
-
-    # ~ def getmeta_default(self, meta_name, default):
-        # ~ meta = self.fs.getmeta(meta_name, default)
-        # ~ if isinstance(meta, basestring):
-            # ~ meta = self.decode_path(meta)
-        # ~ return meta
-
-    # ~ def hasmeta(self, meta_name):
-        # ~ return self.fs.hasmeta(meta_name)
-
-    # ~ def get_contents(self, path, mode="rb"):
-        # ~ path = self.decode_path(path)
-        # ~ data = self.fs.getcontents(path, mode)
-        # ~ return xmlrpclib.Binary(data)
-
-    # ~ def set_contents(self, path, data):
-        # ~ path = self.decode_path(path)
-        # ~ self.fs.setcontents(path, data.data)
-
-    # ~ def exists(self, path):
-        # ~ path = self.decode_path(path)
-        # ~ return self.fs.exists(path)
-
-    # ~ def isdir(self, path):
-        # ~ path = self.decode_path(path)
-        # ~ return self.fs.isdir(path)
-
-    # ~ def isfile(self, path):
-        # ~ path = self.decode_path(path)
-        # ~ return self.fs.isfile(path)
-
-    # ~ def listdir(self, path="./"):
-        # ~ #path = self.decode_path(path)
-        # ~ return self.fs.listdir(path)
-
-    # ~ def makedir(self, path, recursive=False, allow_recreate=False):
-        # ~ path = self.decode_path(path)
-        # ~ return self.fs.makedir(path, recursive, allow_recreate)
-
-    # ~ def remove(self, path):
-        # ~ path = self.decode_path(path)
-        # ~ return self.fs.remove(path)
-
-    # ~ def removedir(self, path, recursive=False, force=False):
-        # ~ path = self.decode_path(path)
-        # ~ return self.fs.removedir(path, recursive, force)
-
-    # ~ def rename(self, src, dst):
-        # ~ src = self.decode_path(src)
-        # ~ dst = self.decode_path(dst)
-        # ~ return self.fs.rename(src, dst)
-
-    # ~ def settimes(self, path, accessed_time, modified_time):
-        # ~ path = self.decode_path(path)
-        # ~ if isinstance(accessed_time, xmlrpclib.DateTime):
-            # ~ accessed_time = datetime.strptime(accessed_time.value, "%Y%m%dT%H:%M:%S")
-        # ~ if isinstance(modified_time, xmlrpclib.DateTime):
-            # ~ modified_time = datetime.strptime(modified_time.value, "%Y%m%dT%H:%M:%S")
-        # ~ return self.fs.settimes(path, accessed_time, modified_time)
-
-    # ~ def getinfo(self, path):
-        # ~ path = self.decode_path(path)
-        # ~ info = self.fs.getinfo(path)
-        # ~ info = dict((k, v) for k, v in info.iteritems()
-                    # ~ if k in self._allowed_info)
-        # ~ return info
-
-    # ~ def desc(self, path):
-        # ~ path = self.decode_path(path)
-        # ~ return self.fs.desc(path)
-
-    # ~ def getxattr(self, path, attr, default=None):
-        # ~ path = self.decode_path(path)
-        # ~ attr = self.decode_path(attr)
-        # ~ return self.fs.getxattr(path, attr, default)
-
-    # ~ def setxattr(self, path, attr, value):
-        # ~ path = self.decode_path(path)
-        # ~ attr = self.decode_path(attr)
-        # ~ return self.fs.setxattr(path, attr, value)
-
-    # ~ def delxattr(self, path, attr):
-        # ~ path = self.decode_path(path)
-        # ~ attr = self.decode_path(attr)
-        # ~ return self.fs.delxattr(path, attr)
-
-    # ~ def listxattrs(self, path):
-        # ~ path = self.decode_path(path)
-        # ~ return [self.encode_path(a) for a in self.fs.listxattrs(path)]
-
-    # ~ def copy(self, src, dst, overwrite=False, chunk_size=16384):
-        # ~ src = self.decode_path(src)
-        # ~ dst = self.decode_path(dst)
-        # ~ return self.fs.copy(src, dst, overwrite, chunk_size)
-
-    # ~ def move(self, src, dst, overwrite=False, chunk_size=16384):
-        # ~ src = self.decode_path(src)
-        # ~ dst = self.decode_path(dst)
-        # ~ return self.fs.move(src, dst, overwrite, chunk_size)
-
-    # ~ def movedir(self, src, dst, overwrite=False, ignore_errors=False, chunk_size=16384):
-        # ~ src = self.decode_path(src)
-        # ~ dst = self.decode_path(dst)
-        # ~ return self.fs.movedir(src, dst, overwrite, ignore_errors, chunk_size)
-
-    # ~ def copydir(self, src, dst, overwrite=False, ignore_errors=False, chunk_size=16384):
-        # ~ src = self.decode_path(src)
-        # ~ dst = self.decode_path(dst)
-        # ~ return self.fs.copydir(src, dst, overwrite, ignore_errors, chunk_size)
 
 
 class RPCFSServer(SimpleXMLRPCServer):
@@ -309,8 +176,7 @@ class RPCFSServer(SimpleXMLRPCServer):
     attribute "serve_more_requests" to False.
     """
     
-    def __init__(self, fs, addr, requestHandler=None, logRequests=False):
-        print(addr)
+    def __init__(self, fs, addr, requestHandler=None):
         kwds = dict(allow_none=True)
         if requestHandler is not None:
             kwds['requestHandler'] = requestHandler
--- tests/test_xmlrpc.py
+++ tests/test_xmlrpc.py
@@ -12,133 +12,16 @@ import base64
 import six
 import fs
 
-from contextlib import closing
-from xml.parsers.expat import ExpatError 
 from fs.expose.xmlrpc import serve
-from six.moves import xmlrpc_client
-import six.moves.xmlrpc_client as xmlrpclib
-from six.moves.xmlrpc_client import Fault
-import six.moves.cPickle as pickle
 from fs.errors import *
-from fs import errors, info
 from fs.test import *
-
+from fs.xmlrpcfs import XMLRPC_FS
 
 from .utils import mock
 
 
-class XMLRPC_Client_FS(object):
-    def __init__(self, *args,**kwargs):
-        self.proxy = xmlrpc_client.ServerProxy(*args,**kwargs)
-
-    def __getattr__(self, name):
-        # magic method dispatcher
-        return xmlrpclib._Method(self.__request, name)
-
-    def __call__(self, attr):
-        """A workaround to get special attributes on the ServerProxy
-           without interfering with the magic __getattr__
-        """
-        if attr == "close":
-            return self.__close
-        elif attr == "transport":
-            return self.__transport
-        raise AttributeError("Attribute %r not found" % (attr,))
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *args):
-        self.__close()
-
-
-
-    def __request(self, methodname, params):
-        
-        #Presendfunctions
-        if methodname in ['getmeta']:
-            if len(params) == 0:
-                params = ('',)
-                
-        if methodname in ['settext','appendtext']:
-            if type(params[1]) == bytes:
-                raise TypeError('Bytes not allowed')
-                
-                
-                
-
-        if six.PY2:
-            if methodname in ['setbytes','appendbytes']:
-
-                if type(params[1]) == unicode:
-                    raise TypeError('Unicode not allowed')
-        
-        
-        if methodname in ['setbytes','appendbytes']:
-            params = (params[0],xmlrpclib.Binary(params[1]))
-
-        #Send
-        func = getattr(self.proxy, methodname)
-        
-        try:
-            data = func(*params)
-            try:
-                print(methodname, params,'-->',data)
-            except:
-                pass
-                
-        except ExpatError as err:
-              raise errors.InvalidCharsInPath(err)
-              
-        except Fault as err:
-            err = str(err)
-            if 'fs.errors' in err:
-                x = err.split('fs.errors.')[1].split("'")[0]
-                errorobj = getattr(errors, x)
-                raise errorobj(err,'')
-            elif 'exceptions.TypeError' in err:
-                raise TypeError(err)
-            elif 'ExpatError' in err:
-                raise errors.InvalidCharsInPath(err)
-            else:
-                print(err)
-                raise
-
-        #Postsendfunctions
-
-
-        if methodname in ['getbytes']:
-            data = data.data
-            
-        if methodname in ['getmeta']:
-            if 'invalid_path_chars' in data:
-                data['invalid_path_chars'] = data['invalid_path_chars'].data.decode('utf-8')
-            
-        if six.PY2:
-            if methodname in ['getinfo','getdetails']:
-                data['basic']['name'] = data['basic']['name'].decode('utf-8')
-            if methodname in ['listdir']:
-                outlist = []
-                for entry in data:
-                    outlist.append(entry.decode('utf-8'))
-                data = outlist
-            
-            if methodname in ['gettext']:
-                #Ugly Hack to let the Tests run through
-                try:
-                    data = data.decode('utf-8')
-                except:
-                    pass
-        if methodname in ['getinfo','getdetails']:
-            data = info.Info(data)
-                
-        return data
-
-
 
 class TestExposeXMLRPC(unittest.TestCase,FSTestCases):
-
-# ~ class TestExposeXMLRPC(unittest.TestCase,FSTestCases):
 
     host = 'localhost'
     port = 8081
@@ -148,13 +31,7 @@ class TestExposeXMLRPC(unittest.TestCase,FSTestCases):
     def setUpClass(cls):
         cls.test_fs = fs.open_fs('mem://')
         cls.server_thread = serve(cls.test_fs, cls.host, cls.port)
-        cls.fs = XMLRPC_Client_FS("http://%s:%s/"%(cls.host,cls.port),allow_none=True)#,verbose=True)
-        print('Warning:')
-        print('info objects are transfered as raw')
-        print('setbytes have to be converted to xmlrpclib.Binary')
-        print('getbytes have to be converted back with xmlrpcbinary.data')
-        print('kwargs must be sent as args')
-        
+        cls.fs = XMLRPC_FS("http://%s:%s/"%(cls.host,cls.port),allow_none=True)#,verbose=True)
 
     def tearDown(self):
         self.test_fs.removetree('/')
@@ -286,7 +163,9 @@ class TestExposeXMLRPC(unittest.TestCase,FSTestCases):
     @unittest.skip("Not Supported")
     def test_filterdir(self):
         #Not Supported
-        pass     
+        pass  
+        
+
 #Modified
 ##############################
 
@@ -322,7 +201,6 @@ class TestExposeXMLRPC(unittest.TestCase,FSTestCases):
         self.assertEqual(_bytes, all_bytes)
         self.assert_bytes('foo', all_bytes)
         with self.assertRaises(TypeError):
-            print('ERROR NOW')
             self.fs.setbytes('notbytes', 'unicode')
             
     def test_copy(self):
